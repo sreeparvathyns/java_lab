f230fbf1ba1993c358185a73584a2a349868358c
sreeparvathyns <sreeparvathynsn@gmail.com>
2025-02-28 15:51:50 +0530

    Add files via upload

---
+++ SymMatrix.java
@@ -0,0 +1,43 @@
+//1. Read a matrix from the console and check whether it is symmetric or not.
+
+import java.util.Scanner;
+class SymMatrix{  
+public static void main(String args[]){  
+int m=3,n=3;
+int i;
+int j;
+int arr[][]=new int[m][n];
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the elements");
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+{
+arr[i][j]=sc.nextInt();
+}}
+
+boolean issymmetric=true;
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+{
+if(arr[i][j]!=arr[j][i])
+{
+issymmetric=false;
+break;
+}
+}
+}
+if(issymmetric==true)
+{
+System.out.println("The matrix is symmetric");
+}
+else
+{
+System.out.println("The matrix is not symmetric");
+}
+
+sc.close();
+
+}}
+
